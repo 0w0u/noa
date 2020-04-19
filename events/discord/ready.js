d42294626db12ef4f49d9851de97ece9b35a2209
wwmon <kmoonsugoi@gmail.com>
2020-04-18 22:32:32 -0500

    Cambio versión arreglo bug ready, función cambiada

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -6,6 +6,11 @@ module.exports = class event {
     let client = this.client,
       prefix = client.config.prefix;
     try {
+      let number = 0;
+      client.guilds.cache.forEach((x) => {
+        number += x.memberCount;
+      });
+      client.userCount = number;
       let elementos = [`❤️ ~ ${client.guilds.cache.size.toLocaleString()} servidores, ${client.userCount.toLocaleString()} usuarios`, `❤️ ~ ${client.config.invite}`, `❤️ ~ ${client.config.dbl}`, `❤️ ~ ${client.config.vote}`, `❤️ ~ ${client.config.donate}`, `❓ ~ ${prefix}help`, `❓ ~ ${client.config.support}`, `❓ ~ ${client.config.github}`, '❓ ~ https://noa.wwmon.xyz', `❓ ~ v${require('../../package.json').version}`, `🦠 ~ ¡Lávate las manos con frecuencia!`, `😷 ~ Protégete a ti y a los demás`, `🏠 ~ Quédate En Casa`, `🏠 ~ #QuédateEnCasa`, `👍 ~ No seas pendejo y quedate en casa para no contagiar a tu familia y tener el maldito remordimiento de haberlos infectado :D`, `😷 ~ Mon no se baña`, `❤️ ~ Te amo Javi`, `👋 ~ Saludos a la Reina Isabel`];
       setInterval(() => {
         client.user.setActivity(elementos[Math.floor(elementos.length * Math.random())]);
