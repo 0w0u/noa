14a6c774ad7c3bc738de0e5024eaef00b15a6402
wwmon <kmoonsugoi@gmail.com>
2020-05-02 12:04:24 -0500

    Botinfo.js

--- commands/Información/botinfo.js
+++ commands/Información/botinfo.js
@@ -19,16 +19,14 @@ module.exports = class command extends require('../../base/models/Command.js') {
     try {
       let msg = await message.channel.send(client.fns.reply('generating', message)),
         jav = await client.users.fetch(client.config.owners[1]),
-        mon = await client.users.fetch(client.config.owners[0]),
-        bot = await client.dbl.getBot(client.config.botID);
+        mon = await client.users.fetch(client.config.owners[0]);
       embed
         .setColor(client.fns.selectColor('lightcolors'))
-        .setAuthor(`Información de ${client.config.bot}`, client.user.avatarURL())
-        .addField('Información', `• Desarrolladores: ${jav.tag} y ${mon.tag}`, true)
-        .addField('<:ncStats:704042636519931994> Estadísticas', '```• Shards: ' + client.shard.count.toLocaleString() + '\n• Servidores: ' + client.guilds.cache.size.toLocaleString() + '\n• Usuarios: ' + client.userCount.toLocaleString() + '\n• Canales: ' + client.channels.cache.size.toLocaleString() + '\n• Votos: ' + bot.points.toLocaleString() + ' (' + bot.monthlyPoints.toLocaleString() + ' este mes)```')
+        .setTitle(`Información de ${client.config.bot}`, client.user.avatarURL())
+        .addField('<:ncStats:704042636519931994> Estadísticas', '```• Shards: ' + client.shard.count.toLocaleString() + '\n• Servidores: ' + client.guilds.cache.size.toLocaleString() + '\n• Usuarios: ' + client.userCount.toLocaleString() + '\n• Canales: ' + client.channels.cache.size.toLocaleString() + '\n• Votos: ' + client.votes.toLocaleString() + ' (' + client.monthlyVotes.toLocaleString() + ' este mes)```')
         .addField('<:ncProgramming:704043125215199339> Programación', `\`\`\`• Lenguaje: JavaScript\n• Librería: discord.js v${require('discord.js').version}\n• Comandos: ${client.commands.size}\n• Memoria en Uso: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}/${(require('os').totalmem() / 1024 / 1024).toFixed(2)} MB\n• Uptime: ${require('moment').duration(client.uptime).format(' D [días], H [horas], m [min], s [segs]')}\`\`\``)
         .addField('Enlaces', `[Invítame <:ncHeartXV:704040272664723559>](https://noa.wwmon.xyz/invite) | [Soporte ❓](https://noa.wwmon.xyz/support) | [Donaciones <:ncHeartXXI:704040566551478282>](https://noa.wwmon.xyz/donate) | [DBL 🤖](https://noa.wwmon.xyz/dbl) | [Vota 📥](https://noa.wwmon.xyz/vote) | [Web (WIP) 🌐](https://noa.wwmon.xyz/)`)
-        .setFooter('Versión: ' + require('../../package.json').version)
+        .setFooter('Versión: ' + require('../../package.json').version + ' | Desarrollado por: ' + jav.tag + ' y ' + mon.tag)
         .setTimestamp();
       msg.edit('** **', { embed });
     } catch (e) {
